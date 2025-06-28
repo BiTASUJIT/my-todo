1f933917b7e0a8643f634c5112d17a953d0b9ef8
BiTASUJIT <shrisujitmohapatra@gmail.com>
2025-06-28 18:39:15 +0530

    each task created as objec

--- main.js
+++ main.js
@@ -11,38 +11,40 @@ const addItem = document.querySelector("#addItem")
 const formTable = document.querySelector("#formTable")
 let table = document.querySelector("#tableView")
 
-const saveItem = () => {    
-    let task = document.getElementById("itemText").value   
-    if(!task){
+const createTask = () => {
+        let task = {
+        item : document.getElementById("itemText").value,
+        time : dateTime()
+    }
+    if(!task.item){
         document.getElementById("noTask").innerHTML = "<span style='color: red;'>Enter Your Task</span>"
-    }else{
-        let formattedDate = dateTime()
+    }else{        
+        document.getElementById("noTask").innerHTML = ""
+        document.getElementById("itemText").value = null
+        displayTask(task)
+    }    
+}
+const displayTask = (task) => {    
         let tr = table.insertRow(0)
         let td1 = tr.insertCell(0)
         let td2 = tr.insertCell(1)
         let td3 = tr.insertCell(2)
-        td1.innerHTML = task
-        td2.innerHTML = formattedDate
+        td1.innerHTML = task.item
+        td2.innerHTML = task.time
         td3.innerHTML = `<button id="deleteBtn" onClick="table.deleteRow(parentNode.parentNode.rowIndex)">Delete</button>`
-        document.getElementById("itemText").value = null
-        document.getElementById("noTask").innerHTML = ""
     }
-    
-}
 
 const dateTime = () => {
     const now = new Date()       
     const day = now.getDate()
     const month = now.getMonth() + 1
     const year = now.getFullYear();
-
     const hours = now.getHours();
     const minutes = now.getMinutes()
     const seconds = now.getSeconds()
     const ampm = hours >= 12 ? 'PM' : 'AM';
     return(`${day}-${month}-${year}, ${hours}:${minutes}:${seconds} ${ampm}`)    
 }
-
-addItem.addEventListener("click", saveItem)
+addItem.addEventListener("click", createTask)
 
 
